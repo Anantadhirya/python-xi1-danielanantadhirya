7c76cf386337f9aea26ca1286a910e54863e1da4
Anantadhirya <anantadhirya@gmail.com>
2021-02-23 10:00:45 +0700

    module

--- Modules/decode.py
+++ Modules/decode.py
@@ -1,28 +1,23 @@
+"""Module untuk melakukan berbagai cipher dan decode"""
+import base64
+
 def cipher(word, key = 0):
+    """Fungsi untuk melakukan caesar cipher"""
     ret = ""
     for i in word:
         if 0 <= ord(i) - ord('a') and ord(i) - ord('a') < 26:
             ret += chr((ord(i)-ord('a')+key)%26 + ord('a'))
         elif 0 <= ord(i) - ord('A') and ord(i) - ord('A') < 26:
-            ret += chr((ord(i)-ord('a')+key)%26 + ord('A'))
+            ret += chr((ord(i)-ord('A')+key)%26 + ord('A'))
         else:
             ret += i
     return ret
 
-def all_cipher(word, search_word = ""):
+def cipher_all(word, search_word = ""):
+    """Fungsi untuk mencoba semua kemungkinan dan mencari search word tertentu"""
     ret = []
     for i in range(26):
         ciphered_word = cipher(word, i)
         if search_word in ciphered_word:
             ret.append(ciphered_word)
     return ret
-
-from fp import pr
-panjang = 41
-pr('', panjang)
-pr('=' * panjang)
-pr('Daftar Perintah', panjang, 'm')
-pr('- cipher(word, key)', panjang)
-pr('- cipher_all(word, search_word)', panjang)
-pr('=' * panjang)
-pr('', panjang)
--- Modules/fp.py
+++ Modules/fp.py
@@ -1,7 +1,11 @@
+"""Module untuk melakukan print dengan mudah"""
+
 def pl(lst):
+    """Fungsi untuk melakukan print sebuah list"""
     print(*lst, sep="\n")
 
 def pr(kalimat, panjang = 0, perataan = 'l', pengisi = ' '):
+    """Fungsi untuk melakukan print sebuah kalimat dengan mengatur perataannya"""
     if perataan == 'l' or perataan == 'left' or perataan == 'kiri':
         print(kalimat, pengisi * max(0, panjang - len(kalimat)))
     elif perataan == 'm' or perataan == 'middle' or perataan == 'tengah':
