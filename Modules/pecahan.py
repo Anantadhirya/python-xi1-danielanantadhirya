30fda561170adffdf74f0a9982a304b2299f3939
Anantadhirya <anantadhirya@gmail.com>
2021-03-03 11:52:34 +0700

    .

--- Modules/pecahan.py
+++ Modules/pecahan.py
@@ -1,4 +1,6 @@
 from math import gcd
+max_digit = 4
+max_cari = 20
 
 def simplify(pembilang, penyebut, keepDivisor1 = False):
     """Fungsi untuk menyederhanakan pecahan"""
@@ -9,9 +11,11 @@ def simplify(pembilang, penyebut, keepDivisor1 = False):
 
 def dtof(angka):
     """'Decimal to Fraction' -> Fungsi untuk mengubah float menjadi pecahan"""
-    digit = len(str(angka).split('.')[1]) if '.' in str(angka) else 0
-    a = [(round(angka*i), i, abs(round(angka*i)/i - angka)) for i in range(1, min(100, 10**digit)+1) if round(round(angka*i)/i, digit) == angka]
-    a = [(simplify(i[0], i[1]), i[2]) for i in a]
-    a = sorted(list(set(a)), key=lambda x: x[1])
-    a = ["{} error={:.2f}%".format(i[0], 100 * i[1] / angka) for i in a]
+    digit = min(max_digit, len(str(angka).split('.')[1]) if '.' in str(angka) else 0)
+    a = []
+    for i in range(1, 10**digit+1):
+        if round(round(angka*i)/i, digit) == round(angka, digit):
+            a.append((simplify(round(angka*i), i), abs(round(angka*i)/i - angka)))
+    a = sorted(list(set(a)), key=lambda x: x[1])[:max_cari]
+    a = ["{} error={:.1e}".format(i[0], i[1]) for i in a]
     return a
