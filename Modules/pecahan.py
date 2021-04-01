821d3264517546dcf0e49d46add86b635ab4efe2
Anantadhirya <anantadhirya@gmail.com>
2021-04-01 10:08:36 +0700

    .

--- Modules/pecahan.py
+++ Modules/pecahan.py
@@ -1,21 +1,68 @@
-from math import gcd
+import math
 max_digit = 4
+digit = 0
 max_cari = 20
+max_pangkat = 3
+sup = {
+    "0": "⁰", "1": "¹", "2": "²", "3": "³", "4": "⁴", "5": "⁵", "6": "⁶",
+    "7": "⁷", "8": "⁸", "9": "⁹", "a": "ᵃ", "b": "ᵇ", "c": "ᶜ", "d": "ᵈ",
+    "e": "ᵉ", "f": "ᶠ", "g": "ᵍ", "h": "ʰ", "i": "ᶦ", "j": "ʲ", "k": "ᵏ",
+    "l": "ˡ", "m": "ᵐ", "n": "ⁿ", "o": "ᵒ", "p": "ᵖ", "q": "۹", "r": "ʳ",
+    "s": "ˢ", "t": "ᵗ", "u": "ᵘ", "v": "ᵛ", "w": "ʷ", "x": "ˣ", "y": "ʸ",
+    "z": "ᶻ", "A": "ᴬ", "B": "ᴮ", "C": "ᶜ", "D": "ᴰ", "E": "ᴱ", "F": "ᶠ",
+    "G": "ᴳ", "H": "ᴴ", "I": "ᴵ", "J": "ᴶ", "K": "ᴷ", "L": "ᴸ", "M": "ᴹ",
+    "N": "ᴺ", "O": "ᴼ", "P": "ᴾ", "Q": "Q", "R": "ᴿ", "S": "ˢ", "T": "ᵀ",
+    "U": "ᵁ", "V": "ⱽ", "W": "ᵂ", "X": "ˣ", "Y": "ʸ", "Z": "ᶻ", "+": "⁺",
+    "-": "⁻", "=": "⁼", "(": "⁽", ")": "⁾"}
+
+def akar_pangkat(pangkat):
+    """Fungsi untuk mengeluarkan string berupa akar pangkat"""
+    if type(pangkat) != str:
+        pangkat = str(pangkat)
+    pangkat = ''.join(sup[i] for i in pangkat)
+    return "{}√".format(pangkat)
 
 def simplify(pembilang, penyebut, keepDivisor1 = False):
     """Fungsi untuk menyederhanakan pecahan"""
-    FPB = gcd(pembilang, penyebut)
+    FPB = math.gcd(pembilang, penyebut)
     pembilang //= FPB
     penyebut //= FPB
     return "{}/{}".format(pembilang, penyebut) if keepDivisor1 or penyebut != 1 else "{}".format(pembilang)
 
 def dtof(angka):
     """'Decimal to Fraction' -> Fungsi untuk mengubah float menjadi pecahan"""
-    digit = min(max_digit, len(str(angka).split('.')[1]) if '.' in str(angka) else 0)
     a = []
     for i in range(1, 10**digit+1):
         if round(round(angka*i)/i, digit) == round(angka, digit):
             a.append((simplify(round(angka*i), i), abs(round(angka*i)/i - angka)))
     a = sorted(list(set(a)), key=lambda x: x[1])[:max_cari]
-    a = ["{} error={:.1e}".format(i[0], i[1]) for i in a]
     return a
+
+def dtor(angka):
+    """'Decimal to Root' -> Fungsi untuk mengubah float menjadi bentuk akar (√)"""
+    a = []
+    for pangkat in range(2, max_pangkat+1):
+        for i in range(2, 10**digit+1):
+            akar = i**(1/pangkat)
+            # bentuk ᵇ√a = angka
+            if round(akar, digit) == round(angka, digit):
+                a.append(("{}{}".format(akar_pangkat(pangkat), i), abs(akar-angka)))
+            # bentuk c + ᵇ√a = angka
+            elif round(abs(round(angka, digit) - round(akar, digit)) % 1) == 0:
+                a.append(("{} + {}{}".format(round(round(angka,digit)-round(akar,digit)), akar_pangkat(pangkat), i), abs(akar-angka)%1))
+    a = sorted(list(set(a)), key=lambda x: x[1])[:max_cari]
+    return a
+        
+
+def uncalc(angka):
+    """Fungsi untuk mengubah angka pembulatan kalkulator menjadi angka sebelum pembulatan"""
+    global digit
+    digit = min(max_digit, len(str(angka).split('.')[1]) if '.' in str(angka) else 0)
+    ret = []
+    ret += dtof(angka)
+    ret += dtor(angka)
+    
+    max_len = max(len(str(i[0])) for i in ret)
+    ret = sorted(list(set([(str(i[0]) + ' '*(max_len-len(str(i[0]))), i[1]) for i in ret])), key=lambda x:x[1])[:max_cari]
+    print(*["{}\t error={:.1e}".format(i[0], i[1]) for i in ret], sep='\n')
+    return
--- Program/jadwal.py
+++ Program/jadwal.py
@@ -1,23 +1,96 @@
 import os
 from datetime import datetime
 jadwal = []
+list_jam = []
+jam_aktif = 0
 jam = []
+
+class day:
+    jam = datetime.now()
+    hari = datetime.now().weekday()
+    jp = 0
+    jpid = 0
+    mapel = []
+    jadwal = []
+    mulai = 0
+    selesai = 0
+    status = ""
+
+today = day()
 
 kode = {'Upacara':'UPCR', 'Fisika':'FIS', 'Matematika Minat':'MTKP', 'Biologi':'BIO',
         'Agama':'AGM', 'PJOK':'PJOK', 'PPKn':'PPKN', 'Bahasa Inggris':'BING',
         'Bahasa Jawa':'BJW', 'Seni Budaya':'SNB', 'Sejarah Indonesia':'SJI',
         'Informatika':'INF', 'Matematika Wajib':'MTKW', 'PKWU':'PKWU', 'Kimia':'KIM',
         'Bahasa Indonesia':'BIND'}
-hari = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu']
+kode_hari = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu']
+
+def open_file():
+    global jam_aktif, jam
+    with open('C:/Users/Anantadhirya/Downloads/Tugas/Kelas XI/python-xi1-danielanantadhirya/Program/data_jadwal.txt', 'r') as data:
+        isi = data.read().split('\n\n')
+        for i in isi[0].split('\n'):
+            tmp2 = i.split('|')
+            jadwal.append([tuple(j.split(':')) for j in tmp2])
+        for i in range(len(jadwal)):
+            for j in range(len(jadwal[i])):
+                jadwal[i][j] = (jadwal[i][j][0], int(jadwal[i][j][1]), int(jadwal[i][j][2]))
+        
+        jam_aktif = int(isi[1])
+
+        for i in isi[2:]:
+            list_jam.append([])
+            for j in i.split('\n'):
+                tmp2 = j.split('|')
+                list_jam[-1].append([tuple(datetime.strptime('{}:00'.format(l), '%H:%M:%S') for l in k.split('-')) for k in tmp2])
+        jam = list_jam[jam_aktif]
+
+def save_file():
+    with open('C:/Users/Anantadhirya/Downloads/Tugas/Kelas XI/python-xi1-danielanantadhirya/Program/data_jadwal.txt', 'w') as data:
+        for baris in jadwal:
+            for idx, pelajaran in enumerate(baris):
+                data.write("{}:{}:{}".format(pelajaran[0], pelajaran[1], pelajaran[2]))
+                if idx != len(baris) - 1: data.write('|')
+            data.write('\n')
+        data.write('\n')
+        for idx2, baris in enumerate(jam):
+            for idx, tjp in enumerate(baris):
+                data.write("{}:{}-{}:{}".format(tjp[0].hour, tjp[0].minute, tjp[1].hour, tjp[1].minute))
+                if idx != len(baris)-1: data.write('|')
+            if idx2 != len(jam)-1: data.write('\n')
 
 def clear():
     os.system('cls')
+    # print("\n"*100)
 
-def get_jp():
-    jp = 1
-    while cmp(datetime.now(), jam[datetime.now().weekday()][jp-1][1]) == -1:
-        jp += 1
-    return jp
+def update():
+    global today, jam
+    jam = list_jam[jam_aktif]
+    today.jam = datetime.now()
+    today.hari = datetime.now().weekday()
+    today.jp = 0
+    today.jpid = -1
+    today.mapel = jadwal[today.hari]
+    today.jadwal = jam[today.hari]
+    today.mulai = today.jadwal[0][0]
+    today.selesai = today.jadwal[today.mapel[-1][2] - 1][1]
+    if today.hari >= 5:
+        today.status = "libur"
+    elif cmp(today.jam, today.mulai) == 1:
+        today.status = "belum mulai"
+    elif cmp(today.jam, today.selesai) == -1:
+        today.status = "sudah pulang"
+    else:
+        today.status = "pelajaran"
+        today.jp = 1
+        while cmp(today.jam, today.jadwal[today.jp-1][1]) == -1:
+            today.jp += 1
+        today.jpid = 0
+        while today.jp > today.mapel[today.jpid][2]:
+            today.jpid += 1
+        if not (cmp(today.jam, today.jadwal[today.jp-1][0]) <= 0 and cmp(today.jam, today.jadwal[today.jp-1][1]) >= 0):
+            today.status = "istirahat"
+    return
 
 def cmp(time1, time2):
     if time1.hour > time2.hour:
@@ -44,105 +117,66 @@ def delta_time(time1, time2):
     else:
         return (time2.hour - time1.hour - 1, time2.minute - time1.minute + 60)
 
-def open_file():
-    with open('C:/Users/Anantadhirya/Downloads/Tugas/Kelas XI/python-xi1-danielanantadhirya/Program/data_jadwal.txt', 'r') as data:
-        isi = data.read().split('\n')
-        for i in isi[:5]:
-            tmp2 = i.split('|')
-            jadwal.append([tuple(j.split(':')) for j in tmp2])
-        for i in range(len(jadwal)):
-            for j in range(len(jadwal[i])):
-                jadwal[i][j] = (jadwal[i][j][0], int(jadwal[i][j][1]), int(jadwal[i][j][2]))
-        for i in isi[6:11]:
-            tmp2 = i.split('|')
-            jam.append([tuple(datetime.strptime('{}:00'.format(k), '%H:%M:%S') for k in j.split('-')) for j in tmp2])
-
-def save_file():
-    with open('C:/Users/Anantadhirya/Downloads/Tugas/Kelas XI/python-xi1-danielanantadhirya/Program/data_jadwal.txt', 'w') as data:
-        for baris in jadwal:
-            for pelajaran in baris:
-                data.write("{}:{}:{}".format(pelajaran[0], pelajaran[1], pelajaran[2]))
-                if pelajaran != baris[-1]: data.write('|')
-            if baris != jadwal[-1]: data.write('\n')
+def center_str(string, length = 0):
+    return ' ' * max(0, (length-len(string))//2) + string + ' ' * max(0, (length-len(string)+1)//2)
 
 def print_jadwal():
-    clear()
-    arr = [[''], ['Senin'], ['Selasa'], ['Rabu'], ['Kamis'], ['Jumat']]
-    for i in range(1, 10+1):
-        arr[0].append(str(i))
-    for i in range(5):
-        tmp2 = 0
-        for j in range(1, 10+1):
-            if j > jadwal[i][-1][2]:
-                arr[i+1].append('')
-            else:
-                while jadwal[i][tmp2][2] < j:
-                    tmp2 += 1
-                arr[i+1].append(kode[jadwal[i][tmp2][0]])
+    nomer = 10
     max_len = 7
-    for i in arr:
-        max_len = max(max_len, max([len(j) for j in i]))
-    for i in range(len(arr)):
-        for j in range(len(arr[i])):
-            arr[i][j] += ' ' * (max_len - len(arr[i][j]))
-    for day in arr:
-        for pelajaran in day:
-            print(pelajaran, end='')
-            print('\t', end='')
-        print()
+    for hari in kode_hari[:5]:
+        max_len = max(max_len, len(hari))
+    for hari in kode.values():
+        max_len = max(max_len, max([len(mapel) for mapel in hari]))
+    max_len += 2
+    final_len = max_len*(nomer+1) + (nomer+2)
+    
+    print('/' + '-' * (final_len-2) + '\\')
+    temp = '|' + ' ' * max_len + '|'
+    for i in range(1, nomer+1):
+        temp += center_str(str(i), max_len) + '|'
+    print(temp)
+    for idx, hari in enumerate(jadwal):
+        print('|' + '-' * (final_len-2) + '|')
+        temp = '|' + center_str(str(kode_hari[idx]), max_len) + '|'
+        for mapel in hari:
+            temp += center_str(kode[mapel[0]], (max_len+1) * (mapel[2]-mapel[1]+1) - 1) + '|'
+        while(len(temp) < final_len):
+            temp += ' '*(final_len-len(temp)-1) + '|'
+        print(temp)
+    print('\\' + '-' * (final_len-2) + '/')
 
 def sekarang():
-    clear()
-    now = datetime.now()
-    hari_ini = now.weekday()
-    print('Sekarang hari {}, jam {}'.format(hari[hari_ini], now.strftime('%H:%M:%S')))
-    if hari_ini >= 5:
+    print('Sekarang hari {}, jam {}'.format(kode_hari[today.hari], today.jam.strftime('%H:%M:%S')))
+    if today.status == "libur":
         print("Tidak ada pelajaran hari ini")
-    else:
-        mulai = jam[hari_ini][0][0]
-        selesai = jam[hari_ini][jadwal[hari_ini][-1][2]-1][1]
-        if cmp(now, mulai) == 1:
-            print("Pelajaran hari ini belum mulai")
-        elif cmp(now, selesai) == -1:
-            print("Pelajaran hari ini sudah selesai")
-        else:
-            jp = get_jp()
-            jp_mulai = jam[hari_ini][jp-1][0]
-            jp_selesai = jam[hari_ini][jp-1][1]
-            if cmp(jp_mulai, now) >= 0 and cmp(now, jp_selesai) >= 0:
-                print("Saat ini jam pelajaran ke {}".format(jp))
-                i = 0
-                while not(jadwal[hari_ini][i][1] <= jp and jp <= jadwal[hari_ini][i][2]):
-                    i += 1
-                print("Mapel: {} ({}-{})".format(jadwal[hari_ini][i][0].upper(), jam[hari_ini][jadwal[hari_ini][i][1]-1][0].strftime('%H:%M'), jam[hari_ini][jadwal[hari_ini][i][2]-1][1].strftime('%H:%M')))
-            else:
-                print("Saat ini istirahat ({}-{})".format(jam[hari_ini][jp-2][1].strftime('%H:%M'), jp_mulai.strftime('%H:%M')))
+    elif today.status == "belum mulai":
+        print("Pelajaran hari ini belum mulai")
+    elif today.status == "sudah pulang":
+        print("Pelajaran hari ini sudah selesai")
+    elif today.status == "istirahat":
+        print("Saat ini istirahat ({}-{})".format(today.jadwal[today.jp-2][1].strftime('%H:%M'), today.jadwal[today.jp-1][0].strftime('%H:%M')))
+    elif today.status == "pelajaran":
+        print("Saat ini jam pelajaran ke {}".format(today.jp))
+        print("Mapel: {} ({}-{})".format(today.mapel[today.jpid][0].upper(), today.jadwal[today.mapel[today.jpid][1]-1][0].strftime('%H:%M'), today.jadwal[today.mapel[today.jpid][2]-1][1].strftime('%H:%M')))
+        durasi_jam, durasi_menit = delta_time(today.jam, today.jadwal[today.mapel[today.jpid][2]-1][1])
+        print("Keterangan: {}{} menit lagi selesai".format('{} jam '.format(durasi_jam) if durasi_jam > 0 else '', durasi_menit))
     print()
 
 def pelajaran_selanjutnya():
-    clear()
-    now = datetime.now()
-    hari_ini = now.weekday()
-    print('Sekarang hari {}, jam {}'.format(hari[hari_ini], now.strftime('%H:%M:%S')))
-    if hari_ini >= 5:
+    print('Sekarang hari {}, jam {}'.format(kode_hari[today.hari], today.jam.strftime('%H:%M:%S')))
+    if today.status == "libur":
         print("Tidak ada pelajaran hari ini")
+    elif today.status == "sudah pulang":
+        print("Pelajaran hari ini sudah selesai")
     else:
-        selesai = jam[hari_ini][jadwal[hari_ini][-1][2]-1][1]
-        if cmp(now, selesai) == -1:
-            print("Pelajaran hari ini sudah selesai")
+        if today.jpid == len(today.mapel) - 1:
+            print("Pelajaran selanjutnya sudah habis")
+            durasi_jam, durasi_menit = delta_time(today.jam, today.selesai)
+            print("Keterangan: {}{} menit lagi pulang".format('{} jam '.format(durasi_jam) if durasi_jam > 0 else '', durasi_menit))
         else:
-            i_next = 0
-            while i_next < len(jadwal[hari_ini]) and cmp(jam[hari_ini][jadwal[hari_ini][i_next][1]-1][0], now) >= 0:
-                i_next += 1
-            if i_next == len(jadwal[hari_ini]):
-                print("Pelajaran selanjutnya sudah habis")
-                durasi_jam, durasi_menit = delta_time(now, selesai)
-                print("Keterangan: {}{} menit lagi pulang".format('{} jam '.format(durasi_jam) if durasi_jam > 0 else '', durasi_menit))
-            else:
-                print("Pelajaran selanjutnya mapel {} ({}-{})".format(jadwal[hari_ini][i_next][0].upper(), jam[hari_ini][jadwal[hari_ini][i_next][1]-1][0].strftime('%H:%M'), jam[hari_ini][jadwal[hari_ini][i_next][2]-1][1].strftime('%H:%M')))
-                jam_mulai = jam[hari_ini][jadwal[hari_ini][i_next][1]-1][0]
-                durasi_jam, durasi_menit = delta_time(now, jam_mulai)
-                print("Keterangan: {}{} menit lagi".format('{} jam '.format(durasi_jam) if durasi_jam > 0 else '', durasi_menit))
+            print("Pelajaran selanjutnya mapel {} ({}-{})".format(today.mapel[today.jpid+1][0].upper(), today.jadwal[today.mapel[today.jpid+1][1]-1][0].strftime('%H:%M'), today.jadwal[today.mapel[today.jpid+1][2]-1][1].strftime('%H:%M')))
+            durasi_jam, durasi_menit = delta_time(today.jam, today.jadwal[today.mapel[today.jpid+1][1]-1][0])
+            print("Keterangan: {}{} menit lagi".format('{} jam '.format(durasi_jam) if durasi_jam > 0 else '', durasi_menit))
     print()
 
 open_file()
@@ -154,6 +188,8 @@ while(True):
     print("- stop (st)")
     print("Masukkan perintah:")
     tmp = input(">>> ").casefold()
+    clear()
+    update()
     if tmp == 'jadwal' or tmp == 'j':
         print_jadwal()
     elif tmp == 'next' or tmp == 'n':
@@ -161,5 +197,4 @@ while(True):
     elif tmp == 'sekarang' or tmp == 's':
         sekarang()
     else:
-        clear()
         break
